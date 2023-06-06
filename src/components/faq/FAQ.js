9e8422cd82671d33521e6af5de3b872465200325
beahackman <hackmanbeatrice@gmail.com>
2023-06-06 13:17:16 -0700

    added about us and faq page

--- src/components/faq/FAQ.js
+++ src/components/faq/FAQ.js
@@ -1,9 +1,33 @@
+import {useState} from "react";
+import SingleQuestion from "./SingleQuestion";
+import { questionData } from "./questions";
 import React from "react";
+import "./FAQ.css";
 
 export default function FAQ() {
+  const[questions, setQuestions] = useState(questionData);
+
   return (
-    <div>
-      <h1>FAQ Page</h1>
+    <div id="HelpBody">
+      <section id="FAQBody">
+        <h1>Frequently Asked Questions</h1>
+        {questions.map((question) => (
+          <SingleQuestion 
+          question={question.question}
+          answer={question.answer} />
+        ))}
+      </section>
+      <section id="ContactBody">
+        <h2>Need more help? Send in your question below!</h2>
+            <form>
+                <section>
+                    <input type="text" name="name" placeholder="John Doe"></input>
+                    <input type="email" name="email" placeholder="johnDoe@email.com"></input>
+                    <input type="submit"></input>
+                </section>
+                <input type="text" name="question" placeholder="I have a question about..."></input>
+            </form>
+      </section>
     </div>
   );
 }
---
+++ src/components/faq/questions.js
@@ -0,0 +1,27 @@
+export const questionData = [
+    {
+        id: 1,
+        question: "Why does Trader Joe’s have their own web app?",
+        answer: "We love our customers and pride ourselves on having a personable and fair shopping experience. We do this best by having our own app!"
+    },
+    {
+        id: 2,
+        question: "How does pickup work?",
+        answer: "Choose a pickup location, and then a Trader Joe’s employee will package it up for you at the desired time."
+    },
+    {
+        id: 3,
+        question: "Why can I no longer find certain items?",
+        answer: "If an item doesn’t pull its weight in our stores, it goes away, making room for another innovative new product."
+    },
+    {
+        id: 4,
+        question: "How long will it take for it to be ready?",
+        answer: "We strive to gather your groceries as efficiently as possible. That being said, depening on store business, it can take anywhere from 5 to 15 minutes."
+    },
+    {
+        id: 5,
+        question: "Do I have to make an account?",
+        answer: "We do encourage you to make an account so that we can better your shopping experience! It can help you by remembering your favorite products, nearby stores, and personalized recommendations!"
+    },
+]
